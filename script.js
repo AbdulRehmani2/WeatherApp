e3b1316df42872c703d9084c715117ee029bf2f3
AbdulRehmani2 <abdulrehmanirfan2238@gmail.com>
2024-02-02 17:13:30 +0500

    Third Task complete

--- script.js
+++ script.js
@@ -1,21 +1,55 @@
 let input = document.querySelector('.search-input');
 let result = document.querySelector('.search-results');
+result.style.display = 'none';
 
-input.addEventListener('input', (e) => {
+input.addEventListener('keyup', (e) => {
     let value = e.target.value.toLowerCase();
-    if(value == 'dublin')
-    {
-        result.style.display = 'block';
-    }
-    else
-    {
-        result.style.display = 'none';
-    }
-})
-
-input.addEventListener('keydown', (e) => {
+    console.log(value);
+    result.innerHTML = "";
+    result.style.display = 'block';
     if(e.key == 'Enter')
     {
         input.value = "";
     }
+    if(value == 'dublin')
+    {
+        let list = createList(cityData);
+        console.log(list)
+        for(let i = 0; i < list.length; i++)
+        {
+            result.appendChild(list[i]); 
+        }
+    }
+    else if(value == '')
+    {
+        result.style.display = 'none';
+    }
+    else
+    {
+        let error = createLi("Invalid city name. Please try again!");
+        error.style.color = 'red';
+        result.appendChild(error);
+    }
+    
 })
+
+let cityData = ["Dublin, IE", "Dublin, California, US", "Dublin, Georgia, US", "Dublin, Texas, US", "Dublin, Ohio, US"];
+
+function createLi(cityName)
+{
+    let li = document.createElement('li');
+    li.classList.add('search-result');
+    li.innerHTML = cityName;
+    return li;
+}
+
+function createList(cityData)
+{
+    let list = [];
+    for(let i = 0; i < cityData.length; i++)
+    {
+        list.push(createLi(cityData[i])); 
+    }
+    console.log(list);
+    return list;
+}
